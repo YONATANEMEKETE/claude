9b9a18fdef8677800be55751df223421f9003ac0
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-09-12 21:40:47 -0700

    Integration responsive

--- src/components/Integrations.tsx
+++ src/components/Integrations.tsx
@@ -21,7 +21,7 @@ import int19 from '../../public/int19.svg';
 
 const Integrations = () => {
   return (
-    <div className="relative max-w-[1400px] min-[1400px]:mx-auto mt-28 mx-4  min-h-[110vh] bg-myfooterbg rounded-2xl overflow-clip">
+    <div className="relative max-w-[1400px] min-[1400px]:mx-auto mt-28 mx-4  min-h-[100vh] bg-myfooterbg rounded-2xl overflow-clip">
       <div className="absolute z-0  inset-0 rounded-2xl">
         <div className="absolute rounded-2xl opacity-20 inset-0 z-0  bg-myaccent bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
         <div className="absolute rounded-2xl z-0 inset-0 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-transparent  to-myfooterbg to-[80%]"></div>
@@ -37,10 +37,10 @@ const Integrations = () => {
           </div>
 
           <div className="space-y-4 text-center">
-            <div className="text-5xl text-white font-default font-semibold max-w-[700px]">
+            <div className="text-xl min-[400px]:text-3xl lg:text-5xl text-white font-default font-semibold max-w-[300px] min-[450px]:max-w-[700px]">
               Dont Replace. Integrate.
             </div>
-            <p className="text-base text-white font-default max-w-[700px]">
+            <p className="text-xs min-[400px]:text-sm lg:text-base text-white font-default max-w-[300px] min-[450px]:max-w-[400px] lg:max-w-[700px]">
               we understand the hussle of replacing the long used tools in your
               process. thats why we integrate tools that you use in your day to
               day life.
